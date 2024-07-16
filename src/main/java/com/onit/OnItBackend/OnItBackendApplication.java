e1dea61d4e23785264a48e1e87137702daa5adfd
Khetha100 <Khethukuthula990@gmail.com>
2024-07-16 22:58:44 +0200

    spring Boot app backend initial code

---
+++ src/main/java/com/onit/OnItBackend/OnItBackendApplication.java
@@ -0,0 +1,13 @@
+package com.onit.OnItBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnItBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnItBackendApplication.class, args);
+	}
+
+}
